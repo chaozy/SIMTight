ca349a074b27c9efc8c12e575a083786c2a7dc68
chaozy <qz312@cam.ac.uk>
2023-02-02 00:21:29 +0000

    update fastzeroing to a loop

--- apps/Samples/FastZeroEval/FastZeroEval.cpp
+++ apps/Samples/FastZeroEval/FastZeroEval.cpp
@@ -22,58 +22,73 @@ void zeroByLoop(uint32_t *mem, uint32_t size)
 int main()
 {
 
-  uint32_t N = 16 * 500;
-  simt_aligned uint32_t a[N], b[N], c[N];
-  puts("Start init\n");
-  // Initialise inputs
-  uint32_t seed = 3;
-  for (int i = 0; i < N; i++) {
-    a[i] = rand15(&seed);
-    b[i] = rand15(&seed);
-    c[i] = rand15(&seed);
-  }
+  uint32_t N = 16 * 100 * 100;
 
-  printStat("Total DRAM Access: ", STAT_SIMT_TOTAL_DRAM_ACCESSES);
-  uint64_t cycleCount1 = pebblesCycleCount(); 
-  zeroByLoop(c, N);  
-  uint64_t cycleCount2 = pebblesCycleCount();
-  uint64_t cycles = cycleCount2 - cycleCount1;
-  puts("zero by loop - cycle count: "); puthex(cycles >> 32);  puthex(cycles); putchar('\n');
-  printStat("Total DRAM Access: ", STAT_SIMT_TOTAL_DRAM_ACCESSES);
-
-
-  VecZero k;
-  k.blockDim.x = SIMTWarps * SIMTLanes;
-  k.size = N;
-  k.b = b;
-
-  cycleCount2 = pebblesCycleCount();
-  noclRunKernel(&k);
-  cycleCount2 = pebblesCycleCount();
-  cycles = cycleCount2 - cycleCount1;
-  puts("zero by kernel - cycle count: "); puthex(cycles >> 32); puthex(cycles); putchar('\n');
-  printStat("Total DRAM Access: ", STAT_SIMT_TOTAL_DRAM_ACCESSES);
-
-  cycleCount1 = pebblesCycleCount();
-  fastZero(&a, N * sizeof(uint32_t));
-  cycleCount2 = pebblesCycleCount();
-  cycles = cycleCount2 - cycleCount1;
-  puts("fast zero - cycle count: "); puthex(cycles >> 32); puthex(cycles); putchar('\n');
-  printStat("Total DRAM Access: ", STAT_SIMT_TOTAL_DRAM_ACCESSES);
-
-
-  bool flag = 0;
-  for (int i = 0; i < N; i++) 
+  // Repeat the simulation for 10 times with increasing data size
+  for (uint32_t i = 0; i < 10; i++)
   {
-    if (a[i] || b[i] || c[i]) 
-    {
-      flag = 1;
-      break;
-    }
-  }
 
-  if (flag) puts("A method is not fully zeroing its memory\n");
-  else puts("All methods successfully zero the corresponding memory\n");
+    // puts("Processing with data size: "); putchar(N*4); puts(" * 100 byte\n");
+    printf("Prcessing with data size: %d * 100 byte\n", N * 4);
+    // printStat("Total DRAM Access: ", STAT_SIMT_TOTAL_DRAM_ACCESSES);
+    
+
+    simt_aligned uint32_t a[N], b[N], c[N];
+    puts("Start init\n");
+    // Initialise inputs
+    uint32_t seed = 3;
+    for (int i = 0; i < N; i++) {
+      a[i] = rand15(&seed);
+      b[i] = rand15(&seed);
+      c[i] = rand15(&seed);
+    }
+
+    uint64_t cycleCount1 = pebblesCycleCount(); 
+    zeroByLoop(c, N);  
+    uint64_t cycleCount2 = pebblesCycleCount();
+    uint64_t cycles = cycleCount2 - cycleCount1;
+    puts("zero by loop - cycle count: "); puthex(cycles >> 32);  puthex(cycles); putchar('\n');
+    // printStat("Total DRAM Access: ", STAT_SIMT_TOTAL_DRAM_ACCESSES);
+
+
+    VecZero k;
+    k.blockDim.x = SIMTWarps * SIMTLanes;
+    k.size = N;
+    k.b = b;
+
+    cycleCount1 = pebblesCycleCount();
+    noclRunKernel(&k);
+    cycleCount2 = pebblesCycleCount();
+    cycles = cycleCount2 - cycleCount1;
+    puts("zero by kernel - cycle count: "); puthex(cycles >> 32); puthex(cycles); putchar('\n');
+    // printStat("Total DRAM Access: ", STAT_SIMT_TOTAL_DRAM_ACCESSES);
+
+    cycleCount1 = pebblesCycleCount();
+    fastZero(&a, N * sizeof(uint32_t));
+    cycleCount2 = pebblesCycleCount();
+    cycles = cycleCount2 - cycleCount1;
+    puts("fast zero - cycle count: "); puthex(cycles >> 32); puthex(cycles); putchar('\n');
+    // printStat("Total DRAM Access: ", STAT_SIMT_TOTAL_DRAM_ACCESSES);
+
+
+    bool flag = 0;
+    for (int i = 0; i < N; i++) 
+    {
+      if (a[i] || b[i] || c[i]) 
+      {
+        flag = 1;
+        break;
+      }
+    }
+
+    if (flag) puts("A method is not fully zeroing its memory\n");
+    else puts("All methods successfully zero the corresponding memory\n");
+
+    puts("\n\n");
+
+    N *= 2;
+  }
+    
   
 
   puts("FINISHED\n");
